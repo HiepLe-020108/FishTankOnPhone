3530b0f02e263aa167c574ddf2362f63518e0608
HiepLe-020108 <hiepchung2002@gmail.com>
2023-05-29 15:03:59 +0700

    1

--- Assets/Scripts/GameManager/GameData.cs
+++ Assets/Scripts/GameManager/GameData.cs
@@ -8,11 +8,15 @@ public class GameData
     public int moneyCount;
     public int completedLevels;
     public SerializableDictionary<string, bool> unlockedFish;
+    public SerializableDictionary<string, bool> buttonInteractableStates;
+    public SerializableDictionary<string, bool> fishObjectsCollected;
 
     public GameData()
     {
         moneyCount = 0;
         completedLevels = 0;
         unlockedFish = new SerializableDictionary<string, bool>();
+        buttonInteractableStates = new SerializableDictionary<string, bool>();
+        fishObjectsCollected = new SerializableDictionary<string, bool>();
     }
 }
--- Assets/Scripts/UI/HomeScene/ShopButtonHandler.cs
+++ Assets/Scripts/UI/HomeScene/ShopButtonHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class ShopButtonHandler : MonoBehaviour
 {
@@ -11,58 +12,76 @@ public class ShopButtonHandler : MonoBehaviour
     private MoneyManager moneyManager;
     [SerializeField] private List<string> tagOfFishType1 = new List<string>();
     [SerializeField] private List<string> tagOfFishType2 = new List<string>();
-    
+
     [SerializeField] private List<SetupButtonBuyFish> _buttons;
     [SerializeField] private List<FishTypeSO> fishTypeSOList = new List<FishTypeSO>();
     [SerializeField] private List<GameObject> fishGameObjectList = new List<GameObject>();
     [SerializeField] private Button buyFishButton;
-    
+
     [SerializeField] private TMP_Text totalNumberOfFishText;
-    
+
     private int TotalFish;
     private int selectedIndex;
+    private Button lastClickedButton;
 
-    void Start()
+    public static event Action<bool> InteractableStateChanged;
+
+    private void Start()
     {
-        
         buyFishButton.interactable = false;
         moneyManager = GameObject.Find("MoneyManager").GetComponent<MoneyManager>();
         for (int i = 0; i < _buttons.Count; i++)
         {
             var index = i;
             fishGameObjectList[index].SetActive(false);
-            _buttons[index].SetupButton(fishTypeSOList[index],() => ShowInfo(index));
+            _buttons[index].SetupButton(fishTypeSOList[index], () => ShowInfo(index));
         }
         buyFishButton.onClick.AddListener(() => AddFish(selectedIndex));
-    } 
-    
-    void ShowInfo(int index)
-    {
-       selectedIndex = index;
-       buyFishButton.interactable = true;
     }
 
-    void AddFish(int index)
+    private void ShowInfo(int index)
+    {
+        selectedIndex = index;
+        buyFishButton.interactable = true;
+
+        if (lastClickedButton != null)
+        {
+            // Restore interaction for the previously clicked button
+            lastClickedButton.interactable = true;
+
+            // Raise the InteractableStateChanged event with true
+            InteractableStateChanged?.Invoke(true);
+        }
+
+        // Disable interaction for the current clicked button
+        var clickedButton = _buttons[index].GetComponent<Button>();
+        clickedButton.interactable = false;
+
+        // Update the last clicked button
+        lastClickedButton = clickedButton;
+
+        // Raise the InteractableStateChanged event with false
+        InteractableStateChanged?.Invoke(false);
+    }
+
+    private void AddFish(int index)
     {
         if (moneyManager.TotalMoney >= fishTypeSOList[index].FishPrice)
         {
             fishGameObjectList[index].SetActive(true);
-            //Here
             moneyManager.SubMoney(fishTypeSOList[index].FishPrice);
+
+            // Disable interaction for the clicked button after adding the fish
+            if (lastClickedButton != null)
+            {
+                lastClickedButton.interactable = false;
+                lastClickedButton = null;
+
+                // Raise the InteractableStateChanged event with true
+                InteractableStateChanged?.Invoke(true);
+            }
         }
-        var fish1 = GameObject.FindGameObjectsWithTag(tagOfFishType1[0]).Length;
-        var fish2 = GameObject.FindGameObjectsWithTag(tagOfFishType1[1]).Length;
-        var fish3 = GameObject.FindGameObjectsWithTag(tagOfFishType2[0]).Length;
 
-        var fishNum = fish1 + fish2 + fish3;
-        totalNumberOfFishText.SetText(fishNum.ToString());
-        TotalFish = fishNum;
-    }
-
-    void AddFish(GameObject fish)
-    {
-        GameObject clone = Instantiate(fish, placeToSpawn, Quaternion.identity);
-        
         var fish1 = GameObject.FindGameObjectsWithTag(tagOfFishType1[0]).Length;
         var fish2 = GameObject.FindGameObjectsWithTag(tagOfFishType1[1]).Length;
         var fish3 = GameObject.FindGameObjectsWithTag(tagOfFishType2[0]).Length;
